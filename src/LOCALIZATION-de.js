e28acecbe5a11753f1b8d004e5f2c29fb9ec8b7e
IrinaSvirkina <isvirkina@issart.com>
2015-09-10 16:36:02 +0500

    Update LOCALIZATION-de.js

--- src/LOCALIZATION-de.js
+++ src/LOCALIZATION-de.js
@@ -30,7 +30,7 @@ Apd.LOCALE.ADDPAGE.PAGECOLUMNS_TITLE = "Seitenspalten";
 Apd.LOCALE.ADDPAGE.PAGEINFORMATION_TITLE = "Seiteninformation";
 Ext.ns("Apd.LOCALE.AUDITLOG");
 Apd.LOCALE.AUDITLOG.ACTIVITY_VALIDATION_TEXT = "Sie müssen mindestens einen Artikel in dieser Gruppe auswählen";;
-Apd.LOCALE.AUDITLOG.ALL_LOGIN_ACCOUNTS = "Alle Login-Accounts";
+Apd.LOCALE.AUDITLOG.ALL_LOGIN_ACCOUNTS = "Alle Login-Accounts";;
 Apd.LOCALE.AUDITLOG.DATE_TO_EXPORT_IN_GMT = "Exportdatum (GMT)";
 Ext.ns("Apd.LOCALE.AUDITLOG.DESTINATION_FOLDER");
 Apd.LOCALE.AUDITLOG.ERRORACCOUNMESSAGE = "Sie dürfen nicht mehr als {0} Login-Accounts auswählen";
